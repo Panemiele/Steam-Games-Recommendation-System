b2aeae87c991233d3bf0a669c8a20313d1df30f5
Reiner409 <72210285+Reiner409@users.noreply.github.com>
2023-09-23 17:46:34 +0200

    First commit, base structure of the project

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Steam-Games-Recommendation-System
+
 [ML-SII] Steam Games Recommendation System
+
+This repository stores a Machine Learning and Intelligent Internet Systems project made by Romeo Reiner and Romualdi Gabriele for the test.
+
+The purpose of the project was the creation of a Steam game Recommender system through standard Machine Learning approaches and classifiers, such as Naive Bayes, Decision Trees and Stochastic Gradient Descent for the sentiment analysis part, while the recommender part is being done with an item-approach recommender system.
+
+The process is implemented in Python and its libraries.
+
+The output of the computations was then visualized through plots, WordCloud and the recommendations given to the users.
+
+Steps:
+
+- Data gathering
+- Data cleaning
+- Text stemming
+- Gaussian Naive Bayes, Multinomial Naive Bayes , Bernoulli Naive Bayes, Stochastic Gradient Descent, Decisions Tree classifiers training on the train and test sets built
+- WordCloud and Plots representation of data
+- Game ranking system based on their reviews
+- Recommending video-games to the user based on their gaming behaviour
+
+An exhaustive review of the whole process is stored within the PDF file.
+  
+
+## Requirements
+
+- Steam API key
+- Kaggle's steam reviews dataset
+  - Downloadable here: https://www.kaggle.com/datasets/andrewmvd/steam-reviews
---
+++ tags_scraper.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import nltk
+from joblib import load
+import requests
+import re
+import urllib.parse
+import time
+from tqdm import tqdm
+import multiprocessing
+
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn import tree
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, f1_score, recall_score
+
+game_info_url = "https://store.steampowered.com/app/"
+
+
+def process_game_id(game_id_method):
+    game_id_method = str(game_id_method)
+    response = requests.get(game_info_url + game_id_method)
+
+    matches = re.findall(r'href="(https://store\.steampowered\.com/tags/en/([^/]+)/)', response.text)
+
+    if not matches:
+        print(f"No match found for game ID {game_id_method}. Check the response:")
+        print(response.text)
+        return game_id_method, []
+
+    extracted_text_res = [urllib.parse.unquote(match[1]) for match in matches]
+    return game_id_method, extracted_text_res
+
+
+def start():
+    # Get steam api key (Maybe not needed for now)
+    # Domain requested: localhost
+
+    # Define the file path
+    file_path = 'api_key.txt'
+
+    try:
+        # Open the file in read mode
+        with open(file_path, 'r') as file:
+            # Read the API key from the file
+            api_key = file.read().strip()
+            print(f"Steam API Key found")
+    except FileNotFoundError:
+        print(f"Api file '{file_path}' not found.")
+
+    # Get the list of games found in the data, and generate a list of categories
+    revs = pd.read_csv('kaggle/dataset.csv')
+    game_ids = revs['app_id'].unique()
+    print(game_ids.size)
+
+    id_tags = {}
+
+    counter = 0
+
+    print("Starting to send to steam the requests")
+
+    # Define the number of processes (adjust as needed)
+    num_processes = multiprocessing.cpu_count()  # Use all available CPU cores
+
+    # Create a pool of worker processes
+    pool = multiprocessing.Pool(processes=num_processes)
+
+    # Create a progress bar with tqdm
+    with tqdm(total=len(game_ids), desc="Processing") as pbar:
+        results = []
+        for game_id in game_ids:
+            result = pool.apply_async(process_game_id, (game_id,))
+            results.append(result)
+            pbar.update(0.2)
+
+        # Wait for all processes to finish and collect the results
+        for result in results:
+            result_data = result.get()
+            if result_data:
+                game_id, extracted_texts = result_data
+                id_tags[game_id] = extracted_texts
+            pbar.update(0.8)
+
+    # Close the pool of worker processes
+    pool.close()
+    pool.join()
+
+    print(id_tags)
+    games_tag_df = pd.DataFrame.from_dict(id_tags, orient='index').transpose()
+    # Save such list
+    games_tag_df.to_csv('games_tags.csv', index=False)
+
+
+
+# Guard to make the call asyncronous
+if __name__ == "__main__":
+    res = input("Analysis already done. Are you sure you'd like to restart? (Y)es/(N)o")
+    if res.lower() == 'n' or res.lower() == 'no':
+        exit(0)
+    print("Starting the analysis for the tags. Putting a 10 seconds delay just to make sure you know what you're doing.")
+    time.sleep(10)
+    start()
